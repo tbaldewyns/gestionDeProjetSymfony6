3022d476e73fac3eb0094c98bc8515828f35f91d
Your Name <you@example.com>
2022-05-05 00:15:19 +0200

    Update 05-05-22

---
+++ migrations/Version20220503220046.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220503220046 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE data_type ADD min_alarm DOUBLE PRECISION DEFAULT NULL, ADD max_alarm DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE data_type DROP min_alarm, DROP max_alarm');
+    }
+}
